23b81950ed1ff51b731a193ffb8c262648d0912e
TomRoum <tomi.roumio@gmail.com>
2025-01-09 21:25:10 +0200

    Update main.cpp
    Parempi versio

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -2,39 +2,44 @@
 #include <cstdlib> // Needed for rand() and srand()
 #include <ctime>   // Needed for time()
 using namespace std;
-int game();
 
-
-int main(){
-    int kerta;
-        kerta = game();
-
-    game();
-    cout << "arvasit " << kerta << "kertaa" << endl;
-    return 0;
-    }
-
-
-int game() {
-        srand(time(0));
+int game(int kerta) {
+    srand(time(0));
     int randomNum = rand() % 21;
     int arvaus = 0;
-    int kerta = 0;
-    cout << "arvaa mitä numeroa mietin välillä 0-20" << " [DEBUG]: " << randomNum << endl;
-    while(randomNum != arvaus){
-    cin >> arvaus;
-
-    if (arvaus < randomNum) {
-            cout << "väärä numero, mietin suurempaa numeroa " << endl;
-            cout << "kokeile uudestaan" << endl;
+    kerta = 0;
+    cout << "arvaa mitä numeroa mietin välillä 0-20" << endl; //<< " [DEBUG:" << randomNum << "]" << endl;
+    while(randomNum != arvaus){ //toistaa, kunnes arvaat oikein
+        cin >> arvaus;
+        if (cin.fail()) { //basic input sanitation
+            cout<<"anna numero!"<<endl;
+            cin.clear();
+            cin.ignore();
+            cin >> arvaus;
         }
-        if (arvaus > randomNum) {
-            cout << "väärä numero, mietin pienempäää numeroa " << endl;
-            cout << "kokeile uudestaan" << endl;
-
+        if (arvaus < 0 || arvaus > 20) {
+            cout << "anna numero joka on välillä 0-20!" << endl;
+            cin.clear();
+            cin.ignore();
+            cin >> arvaus; //don't try that again
         }
-    kerta ++;
+        if (arvaus < randomNum) {   //onko pienempi
+            cout << "väärä numero, mietin SUUREMPAA numeroa. ";
+            cout << "kokeile uudestaan!" << endl;
+        }
+        if (arvaus > randomNum) {   //onko suurempi
+            cout << "väärä numero, mietin PIENEMPÄÄ numeroa! ";
+            cout << "kokeile uudestaan!" << endl;
+        }
+
+        kerta ++; //keeping count
     }
-    cout << "oikein! mietin suurempaa numeroa " << endl;
-    return kerta;
+    cout << "Oikein! ";
+    return kerta; //palauttaa "main()" funktiolle luvun montako kertaa on arvattu
+}
+int main(){
+    int kerta = 0;
+    kerta = game(kerta); //ottaa "kerta" muuttujan "game()" funtiolta ja asettaa sen "kerta" muuttujan arvoksi
+    cout << "Arvasit " << kerta << " kertaa";
+    return 0;
 }
